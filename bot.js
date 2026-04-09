3245c4e48fa500c83e7b8a2c9df167da980bdab5
Alpha-xTeam <alphateam.dev1234@gmail.com>
2026-04-09 04:52:21 +0300

    fix: use youtube web client with remote components

--- bot.js
+++ bot.js
@@ -295,7 +295,8 @@ function buildYoutubeDlpArgs(extraArgs = '') {
   // Always check current variable value which is updated at startup
   const baseArgs = [
     buildYtDlpBaseArgs(resolveCookiesFile('youtube')),
-    '--extractor-args "youtube:player_client=tv_embedded,android,ios,mweb"',
+    '--extractor-args "youtube:player_client=web"',
+    '--remote-components ejs:github',
     '--js-runtimes deno',
     '--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0 Safari/537.36"'
   ];
